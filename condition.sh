9e8869c7cfe31901c11ee3f6cefd440087319d5a
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 19:22:14 +0530

    asdgff

--- condition.sh
+++ condition.sh
@@ -1,11 +1,11 @@
 num=$1
 string=$2
 
-if [ "${num}" -eq 10 ]; then
+if [ ${num} -eq 10 ]; then
 echo "number is number"
 fi
 
-if [ "${string}" == abc ]; then
+if [ ${string} == abc ]; then
 echo "string is string"
 else
 echo "string is not a valid abc string"
