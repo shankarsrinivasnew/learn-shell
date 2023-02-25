548db33ed23bede29c313b6492151dad4e7c4477
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 18:36:27 +0530

    sdf

--- func.sh
+++ func.sh
@@ -1,6 +1,13 @@
 xyz () {
     echo hello function
+    echo first arg $1
+    echo secong arg $2
+    echo thrid arg $3
+    echo all arg $*
+    echo all arg $@
+    echo number of arguments $#
+
 }
 
 # calling function
-xyz
+xyz hello shankar srinivas fuction
