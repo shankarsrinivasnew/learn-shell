33d7c89c76017826f782821ed25f15f8f3edfc51
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 19:19:53 +0530

    sdghd

--- condition.sh
+++ condition.sh
@@ -1,11 +1,11 @@
-num = $1
-string = $2
+num=$1
+string=$2
 
 if [ "${num}" -eq 10 ]; then
 echo "number is number"
 fi
 
-if [ "${string}" == "abc" ]; then
+if [ "${string}" == abc ]; then
 echo "string is string"
 fi
 
