38c2d7339abdb185908563d6df2beef7a186ff44
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 17:21:52 +0530

    asdfgg

--- print.sh
+++ print.sh
@@ -1,1 +1,8 @@
 echo -e "\e[31m hello world \e[0m "
+echo -e "\e[32m hello world \e[0m "
+echo -e "\e[33m Shankar srinivas \e[0m"
+echo -e "\e[34m Shankar srinivas \e[0m"
+echo -e "\e[35m Shankar srinivas \e[0m"
+echo -e "\e[36m Shankar srinivas \e[0m"
+echo -e "\e[37m Shankar srinivas \e[0m"
+
