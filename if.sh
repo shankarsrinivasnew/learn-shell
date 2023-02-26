eb6f8717cf03985833bc46e0ca656ddaee2073d2
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 16:36:50 +0530

    sfg

--- if.sh
+++ if.sh
@@ -1,9 +1,9 @@
 read -p " enter value 1 or 2 or 3 for if-else-if:" var
-if [ ${var} eq 1 ];then
+if [ ${var} -eq 1 ];then
     echo you took one good
-elif [ ${var} eq 2 ]; then
+elif [ ${var} -eq 2 ]; then
     echo you took two good
-elif [ ${var} eq 3 ]; then
+elif [ ${var} -eq 3 ]; then
     echo you took three good
 else 
     echo you took none idiot
