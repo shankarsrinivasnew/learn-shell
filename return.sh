027defe92ff1ee97e5deefcdc01ed7563e78772d
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 16:17:35 +0530

    dfhd

---
+++ return.sh
@@ -0,0 +1,12 @@
+abc () {
+    echo hello 1
+    echo hello 2
+    return 10
+    echo hello 3
+}
+
+abc
+echo exit status of abc fuction : $?
+echo hello 4
+echo hello 5
+echo exit status of abc fuction
