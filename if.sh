aa675d26e5182656f6c7ea685c8eed586eeb7102
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 16:37:58 +0530

    sfdg

--- if.sh
+++ if.sh
@@ -1,4 +1,4 @@
-read -p " enter value 1 or 2 or 3 for if-else-if:" var
+read -p " enter number 1 or 2 or 3 for if-else-if:" var
 if [ ${var} -eq 1 ];then
     echo you took one good
 elif [ ${var} -eq 2 ]; then
