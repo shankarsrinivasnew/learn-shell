26841804a90499502026c53fa0508aa2430f5208
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 16:33:26 +0530

    sdfgh

--- for.sh
+++ for.sh
@@ -2,3 +2,15 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
     echo "insdie for loop $i"
     sleep 1
 done
+
+read -p " enter value 1 or 2 or 3 for if-else-if:" var
+if [ ${var} eq 1 ];then
+    echo you took one good
+elif [ ${var} eq 2 ]
+    echo you took two good
+elif [ ${var} eq 3 ]
+    echo you took three good
+else 
+    echo you took none idiot
+fi
+
